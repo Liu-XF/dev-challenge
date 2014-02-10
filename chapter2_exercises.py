219b574ec78861fb01048c20a713c2bc34b87d91
Liu-XF <java218@gmail.com>
2014-02-09 22:18:50 -0500

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,16 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Xuefeng Liu
+#2.1 Answer: it recognize a number starting with 0 as a Octal number, and convert it into decimal number, so 02132 = 0*8**4+2*8**3+1*8**2+3*8**1+2*8**0 = 1114
+#2.2 Answers: first part, output is '5 6'; second part 'print (x=5)' will lead to a syntax error'
+#2.3 Answers: 
+#1. value: 8； type: integer; 
+#2. value:8.5; type: float; 
+#3. value:4.0; type:float;
+#4. value:11; type: integer; 
+#5. value:'.....'; type :string
+
+#2.4 Answers: 
+#1: 4.0/3*3.1415*5**3 = 523.58; 
+#2: 24.95*(1-0.4)+3+(60-1)*75 = 4442.97; 
+#3: total time  = ((8*60+15)+(7*60+12)*3+(8*60+15))/60 = 38 minutes and ((8*60+15)+(7*60+12)*3+(8*60+15))-38*60 = 6 seconds ; breakfast at 7 hours 52+38-60 = 30 minutes and 6 seconds
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,57 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Xuefeng Liu
+
+#3.1 Answers: Name error: 
+#  Traceback (most recent call last):
+#  File "<stdin>", line 1, in <module>
+#  NameError: name 'repeat_lyrics' is not defined
+
+#3.2 Answers: runs normally
+# I'm a lumberjack, and I'm okay.
+#I sleep all night and I work all day.
+#I'm a lumberjack, and I'm okay.
+#I sleep all night and I work all day. 
+
+#3.3 Answers: 
+#
+ def right_justify (string):
+ print (' '* (70-len(string))+string)
+
+ right_justify('allen')
+
+#3.4 Answers
+#1.
+def do_twice(f):
+    f()
+    f()
+
+def print_spam():
+    print 'spam'
+do_twice(print_spam)
+
+#result: spam spam
+
+#2. 
+def do_twice(f,value):
+	f(value)
+	f(value) 
+	
+#3#  
+def print_twice(st):
+	print (st)
+	print  (st)
+	
+#4
+ do_twice(print_twice,'spam')
+#result: spam
+#        spam
+#		spam
+#		spam
+
+#5 
+def do_four(f,value):
+	do_twice(f,value)
+	do_twice(f,value)
+
+	
